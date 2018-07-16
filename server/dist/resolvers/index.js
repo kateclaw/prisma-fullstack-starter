527cb6cbe51d1039e002207168f0f64d7cdf491f
kylepecci <kyle.pecci@gmail.com>
2018-07-16 20:35:07 +0100

    backend

--- server/dist/resolvers/index.js
+++ server/dist/resolvers/index.js
@@ -9,15 +9,12 @@ var __assign = (this && this.__assign) || Object.assign || function(t) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var prisma_binding_1 = require("prisma-binding");
-var Viewer_1 = require("./Viewer");
 var Query_1 = require("./Query");
 var auth_1 = require("./Mutation/auth");
-var tweet_1 = require("./Mutation/tweet");
 var AuthPayload_1 = require("./AuthPayload");
 exports.resolvers = {
     Query: Query_1.default,
-    Mutation: __assign({}, auth_1.auth, tweet_1.default),
-    Viewer: Viewer_1.default,
+    Mutation: __assign({}, auth_1.auth),
     AuthPayload: AuthPayload_1.AuthPayload
 };
 exports.fragmentReplacements = prisma_binding_1.extractFragmentReplacements(exports.resolvers);
