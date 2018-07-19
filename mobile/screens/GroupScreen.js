6dbe57db6d4c02d5547986b88efe083eb1b2f963
Blaise <blaise.kellymcdwyer@yahoo.co.uk>
2018-07-19 16:37:59 +0100

    updates to groupscreen

--- mobile/screens/GroupScreen.js
+++ mobile/screens/GroupScreen.js
@@ -3,49 +3,69 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import { View, Button } from "react-native";
 import Post from "../components/Post";
+// import CreateGroupScreen from "../CreateGroupScreen"
 // import CreateGroupScreen from "./CreateGroupScreen";
+
+const GET_GROUPS = gql`
+  query {
+    me {
+      groups {
+        name
+      }
+    }
+  }
+`;
 
 export default class GroupScreen extends React.Component {
   static navigationOptions = {
     drawerLabel: "Groups"
   };
 
-  //    const GET_GROUPS =gql``
-
   render() {
     return (
       <View>
-        {/* <View>
-                     <Query 
-                     variables={{ 
-                         email: localStorage.getItem("email")
-                       }}
-                     query= {GET_GROUPS}>
-                         {({ loading, error, data, refetch }) => {
-                             if (loading) {
-                                  return "Loading..."
-                                }
-    
-                              if (error) {
-                                 return "Uh oh something ain't right"
-                                }
-    
-                             return ( 
-                                    <View> 
-                                      {data.groups.map(post => {
+        <View>
+          <Query
+            // onLoad={async () =>{
+            //  const name = await AsyncStorage.getItem("");
+            // }}
+            //  variables={{
+            //      email: email
+            //     }}
+            query={GET_GROUPS}
+          >
+            {({ loading, error, data }) => {
+              if (loading) {
+                return "Loading...";
+              }
+
+              if (error) {
+                return "Uh oh something ain't right";
+              }
+              console.log(`HI ${data.me}`);
+
+              return (
+                <View>
+                  {/* <CreateGroupScreen refetchGroups={refetch}/> */}
+                  {/* {data.groups.map(post => {
                                           return (
                                              <Post
-                                                 key={tweet.id}
-                                                 text={tweet.text}
-                                                 author={tweet.author}
+                                                 key={post.id}
+                                                 text={post.text}
+                                                 author={post.author}
                                                 />
                                             )
-                                        })}
-                                   </View>
-                                )
-                            }}
-                       </Query>
-                    </View> */}
+                                        })} */}
+
+                  {data.me.groups.map(display => {
+                    return <Text key={display.groups}>{display.name}</Text>;
+                  })}
+                </View>
+              );
+            }}
+          </Query>
+        </View>
+
         <Button
           title="Create Group Screen"
           onPress={() => {
